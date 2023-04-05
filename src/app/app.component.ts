06383a863de5857872bb88bba3a9f346fe230e84
Sarah Susan <55784685+velvetrose@users.noreply.github.com>
2023-04-05 11:03:52 +0530

    project test

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'fylo-landing-page-with-two-column-layout';
+}
